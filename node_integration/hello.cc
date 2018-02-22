60e6821296c6108c4284497d7ccc88797f5c4cc2
Xing <xingmarc@yeah.net>
2018-02-21 23:56:31 -0800

    Using napi instead of v8.

--- node_integration/hello.cc
+++ node_integration/hello.cc
@@ -1,25 +1,54 @@
-// hello.cc
-#include <node.h>
+// hello.cc using N-API
+#include <node_api.h>
 
 namespace demo {
 
-using v8::FunctionCallbackInfo;
-using v8::Isolate;
-using v8::Local;
-using v8::Object;
-using v8::String;
-using v8::Value;
+napi_value Method(napi_env env, napi_callback_info args) {
+  napi_value greeting;
+  napi_status status;
 
-void Method(const FunctionCallbackInfo<Value>& args) {
-  Isolate* isolate = args.GetIsolate();
-  args.GetReturnValue().Set(String::NewFromUtf8(isolate, "world"));
+  status = napi_create_string_utf8(env, "hello", 6, &greeting);
+  if (status != napi_ok) return nullptr;
+  return greeting;
 }
 
-void init(Local<Object> exports) {
-  NODE_SET_METHOD(exports, "hello", Method);
+
+/*
+napi_value Method2(napi_env env, napi_callback_info args) {
+  napi_value result;
+  napi_status status;
+
+  status = napi_create_int32(env, args[0] + args[1], &result);
+  if (status != napi_ok) return nullptr;
+  return greeting;
+}
+*/
+
+napi_value init(napi_env env, napi_value exports) {
+  napi_status status;
+  napi_value fn;
+
+  status = napi_create_function(env, nullptr, 0, Method, nullptr, &fn);
+  if (status != napi_ok) return nullptr;
+
+
+/*
+  napi_value fn2;
+
+  status = napi_create_function(env, nullptr, 0, Method2, nullptr, &fn2);
+  if (status != napi_ok) return nullptr;
+*/
+
+  status = napi_set_named_property(env, exports, "hello", fn);
+  if (status != napi_ok) return nullptr;
+  /*
+  status = napi_set_named_property(env, exports, "add", fn2);
+  if (status != napi_ok) return nullptr;
+  */
+  return exports;
 }
 
-NODE_MODULE(NODE_GYP_MODULE_NAME, init)
+NAPI_MODULE(NODE_GYP_MODULE_NAME, init)
 
 }  // namespace demo
 
