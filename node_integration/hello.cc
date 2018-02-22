6fd825e36edebe3097d29ccf6e26bd5df3cfa54a
Xing <xingmarc@yeah.net>
2018-02-22 00:23:35 -0800

    My own function using napi: add two integers.

--- node_integration/hello.cc
+++ node_integration/hello.cc
@@ -13,16 +13,31 @@ napi_value Method(napi_env env, napi_callback_info args) {
 }
 
 
-/*
 napi_value Method2(napi_env env, napi_callback_info args) {
   napi_value result;
   napi_status status;
 
-  status = napi_create_int32(env, args[0] + args[1], &result);
+  napi_value argv[2];
+  napi_value thisArg;
+  size_t argc = 2;
+ 
+  status = napi_get_cb_info(env, args, &argc, argv, &thisArg, nullptr);
+  if (status != napi_ok)
+	  return nullptr;
+  int num1 = 0, num2 = 0;
+  status = napi_get_value_int32(env, argv[0], &num1);
+  if (status != napi_ok) {
+        napi_throw_error(env, NULL, "Invalid number was passed as argument");
+    }
+  status = napi_get_value_int32(env, argv[1], &num2);
+  if (status != napi_ok) {
+        napi_throw_error(env, NULL, "Invalid number was passed as argument");
+    }
+  int sum = num1 + num2;
+  status = napi_create_int32(env, sum, &result);
   if (status != napi_ok) return nullptr;
-  return greeting;
+  return result;
 }
-*/
 
 napi_value init(napi_env env, napi_value exports) {
   napi_status status;
@@ -32,19 +47,15 @@ napi_value init(napi_env env, napi_value exports) {
   if (status != napi_ok) return nullptr;
 
 
-/*
   napi_value fn2;
 
   status = napi_create_function(env, nullptr, 0, Method2, nullptr, &fn2);
   if (status != napi_ok) return nullptr;
-*/
 
   status = napi_set_named_property(env, exports, "hello", fn);
   if (status != napi_ok) return nullptr;
-  /*
   status = napi_set_named_property(env, exports, "add", fn2);
   if (status != napi_ok) return nullptr;
-  */
   return exports;
 }
 
--- node_integration/hello.js
+++ node_integration/hello.js
@@ -2,5 +2,6 @@
 const addon = require('./build/Release/addon');
 
 console.log(addon.hello());
+console.log(addon.add( 100, 1 ));
 // Prints: 'world'
 
